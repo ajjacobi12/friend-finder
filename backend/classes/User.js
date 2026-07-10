c7b4bdf0c334ec68d86d94d258ae9106c54720ac
ajjacobi12 <ajjacobi12@gmail.com>
2026-07-10 01:38:20 -0600

    Created class function. Oh boy, the fun of refactorization begins...

---
+++ backend/classes/User.js
@@ -0,0 +1,48 @@
+//backend/classes/User.js
+
+export class User {
+    constructor( data ) {
+        this.uuid = data.uuid;
+        this.socketID = data.socketID;
+        this.sessionID = data.sessionID;
+        this.name = data.name || null;
+        this.color = data.color || null;
+        this.isHost = data.isHost;
+        this.isRegistered = data.isRegistered || false;
+        this.status = data.status || 'online';
+        this.lastSeen = data.lastSeen || Date.now();
+    }
+
+    updateProfile(newProfile) {
+        if(newProfile.name) this.name = newProfile.name;
+        if(newProfile.color) this.color = newProfile.color;
+    }
+
+    isOnline() {
+        return this.status === 'online' && this.isRegistered;
+    }
+
+    getName() {
+        return this.name || `User-${this.uuid.slice(-4)}`;
+    }
+
+    setOnline() {
+        this.lastSeen = Date.now();
+        this.status = 'online';
+    }
+
+    setOffline() {
+        this.lastSeen = Date.now();
+        this.status = 'offline';
+    }
+
+    toJSON() {
+        return {
+            uuid: this.uuid,
+            name: this.name,
+            color: this.color,
+            isHost: this.isHost,
+            status: this.status
+        };
+    }
+}
